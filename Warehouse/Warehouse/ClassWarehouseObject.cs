57de4cfaee360b74e7642581be1a82a27d1d25fa
Ars <76564343+ArsenyStahkov@users.noreply.github.com>
2023-11-08 01:54:51 +0500

    SetPallet
    1) Method SetPallet() was expanded.
    2) The program was split into 2 modules.

---
+++ Warehouse/Warehouse/ClassWarehouseObject.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Warehouse
+{
+    partial class WarehouseObject
+    {
+        private const byte _EMPTY_CELL = 0;
+        private const uint _MAX_X = 70;      // Pallets + cranes + empty space (boxes are since line 6)
+        private const uint _MAX_Y = 60;      // Pallets (36) + reserve + empty space
+        private const uint _MAX_Z = 8;
+        private const int _ID_GLUE = 1;
+        private const int _ID_CONSTRUCTION_MATERIALS = 2;
+        private const int _ID_GROUT = 3;
+        private const int _ID_FASTENERS = 4;
+        private const int _ID_HOUSEHOLD_GOODS = 5;
+        private const int _ID_DETERGENTS = 6;
+        private const int _ID_AUTO_CHEMISTRY = 7;
+        private const int _ID_HAND_TOOLS = 8;
+        private const int _ID_ELECTRICAL_EQUIPMENT = 9;
+        private const int _ID_AUTOMOTIVE_GOODS = 10;
+
+        private int[,,] _coordinates;
+
+        WarehouseObject()
+        {
+            _coordinates = new int[_MAX_X, _MAX_Y, _MAX_Z];
+
+            // Declare cranes
+            int[] freeCranesArray = new int[5] { 11, 12, 13, 14, 15 };
+            int[] busyCranesArray = new int[5] { -11, -12, -13, -14, -15 };
+
+            //for (int i = 0; i <= _CRANES_COUNT; i++)
+            //    _coordinates[i * 10, 0, 0] = _ID_CRANE_FREE;
+
+            //_coordinates[20, 0, 0] = _ID_2_CRANE_FREE;
+            //_coordinates[30, 0, 0] = _ID_3_CRANE_FREE;
+            //_coordinates[40, 0, 0] = _ID_4_CRANE_FREE;
+            //_coordinates[50, 0, 0] = _ID_5_CRANE_FREE;
+        }
+    }
+}
---
+++ Warehouse/Warehouse/Methods.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warehouse
+{
+    partial class WarehouseObject
+    {
+        private void SetPallet(int crane_X, int crane_Y, int crane_Z, int id_Crane, int id_Pallet)
+        {
+            try
+            {
+                int initial_X = crane_X;
+                int initial_Y = crane_Y;
+                int initial_Z = crane_Z;
+
+                if (_coordinates[crane_X, crane_Y, crane_Z] != id_Crane)
+                {
+                    Console.WriteLine("The number of crane is incorrect!");
+                    return;
+                }
+
+                if (crane_Z != 0)
+                {
+                    Console.WriteLine("Z coordinate is incorrect!");
+                    return;
+                }
+
+                while (crane_X < 5)
+                    crane_X++;
+
+                // While there are pallets on the right from the crane, move it down (X coordinate)
+                while ((_coordinates[crane_X, crane_Y - 1, crane_Z] != _EMPTY_CELL) && (crane_X != _MAX_X))
+                    crane_X++;
+
+                // Move crane left until it is next to a pallet or the wall
+                for (int i = 0; i < 10; i++)
+                {
+                    if (_coordinates[crane_X, crane_Y - 1, crane_Z] != _EMPTY_CELL)
+                        break;
+                    crane_Y--;
+                }
+
+                // If the adjacent cell is busy, try to put the pallet upper (Z coordinate)
+                for (int i = 0; i < _MAX_Z; i++)
+                {
+                    if (_coordinates[crane_X, crane_Y - 1, i] == _EMPTY_CELL)
+                    {
+                        _coordinates[crane_X, crane_Y - 1, i] = id_Pallet;
+
+                        // Return crane on the initial place but with opposite id
+                        _coordinates[initial_X, initial_Y, initial_Z] = id_Crane * -1;
+                    }
+                }
+
+                return;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        //private int GetPallet(int X, int Y, int Z, int crane_Z, int id_Pallet)
+        //{
+        //    try
+        //    {
+        //        int id_Box = _ID_CRANE_BUSY;
+        //        if (_coordinates[X, Y, Z] != _ID_CRANE_FREE)
+        //            return id_Box;
+
+        //        while (X < 5)
+        //            ++X;
+
+        //        while (_coordinates[X, Y - 1, Z] == _EMPTY_CELL)
+        //            Y--;
+
+        //        // If the adjacent cell is free, try to take the pallet on Z (upper)
+        //        for (int i = 0; i < _MAX_Z; i++)
+        //        {
+        //            if (_coordinates[X, Y - 1, i] == _ID_CRANE_BUSY && ((Y - 1) % 10 != 0))
+        //            {
+        //                id_Box = _coordinates[X, Y - 1, i];
+        //                _coordinates[X, Y - 1, i] = _EMPTY_CELL;
+        //                _coordinates[X, Y, i] = _ID_CRANE_BUSY;
+        //            }
+        //        }
+
+        //        return id_Box;
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        throw new Exception(ex.Message);
+        //    }
+        //}
+
+        private void ReplacePallet(int box_X, int box_Y, int box_Z, int target_X, int target_Y, int target_Z)
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            WarehouseObject warehouseObject = new WarehouseObject();
+
+        }
+    }
+}
